ebe331706e7e61401008f2c140ce2ce4efb8a4d5
Melih DİNÇER <m_dincer41@hotmail.com>
2025-01-17 17:35:45 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Calculator from './components/Calculator'
+import CalculatorUseReducer from './components/CalcutatorUseReducer'
+function App() {
+
+  return (
+    <div className='App'>
+      <Calculator></Calculator>
+      <CalculatorUseReducer></CalculatorUseReducer>
+    </div>
+  )
+}
+
+export default App
