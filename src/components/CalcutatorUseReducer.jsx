b1aff8f3044f0b046960e1a7dc47d0cd7f793880
Melih DİNÇER <m_dincer41@hotmail.com>
2025-01-18 16:41:15 +0300

    Kod düzenlemesi adına fonksiyon farklı bir js içerisine alındı.

--- src/components/CalcutatorUseReducer.jsx
+++ src/components/CalcutatorUseReducer.jsx
@@ -1,26 +1,7 @@
 import { useReducer } from "react";
 import "./Calculator.css"
+import { initialState, reducer} from "../reducers/reducerCalculator"
 const CalculatorUseReducer = () => {
-
-  function reducer(state, action){
-    switch(action.type){
-      case "SET_NUMBER1":
-        return {...state, number1: action.payload};
-        case "SET_NUMBER2":
-        return {...state, number2: action.payload};
-        case "SET_RESULT":
-        return {...state, result: action.payload};
-        default:
-          throw new Error("Invalid action type.");
-
-    }
-  };
-
-  const initialState = {
-    number1:0,
-    number2:0,
-    result:0
-  };
   const [state, dispatch] = useReducer(reducer, initialState);
   console.log(state);
 
